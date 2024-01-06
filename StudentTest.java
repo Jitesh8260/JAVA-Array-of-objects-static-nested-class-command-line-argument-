246cc80e172970326196e605e83742f374439e6d
Jitesh Bhakat <137819786+Jitesh8260@users.noreply.github.com>
2024-01-06 22:21:42 +0530

    Update StudentTest.java

--- StudentTest.java
+++ StudentTest.java
@@ -25,7 +25,7 @@ public class StudentTest{
             int pin = scanner.nextInt();
 
             Student student = new Student(name, roll, city, pin);
-            student.displa yStudent();
+            student.displayStudent();
             System.out.println();
         }
 }
